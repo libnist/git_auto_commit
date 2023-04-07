36ac825a11fb22845bd8cf12f580014132d4e19a
libnist <ghahremani.mim@gmail.com>
2023-04-07 14:23:43 +0330

    Complete commenting gac.sh & update.sh files.

--- gac.sh
+++ gac.sh
@@ -8,8 +8,11 @@
 # Automatically make one commit per day in GitHub.
 
 # Usage:
-# still none.
+# 1) cd to the directory of the repo.
+# 2) ./gac.sh -d Direcory/of/your/choice -u your_github_repo.git
+# if you don't pass d, working directory wi'll be used instead.
 
+# Parsing parameters
 while getopts d:u: flag
 do
     case "${flag}" in
@@ -18,16 +21,18 @@ do
     esac
 done
 
+# if d is not specified, current working directory wi'll be used instead
 if [ -z "$dir" ] 
 then
     # Set initial values
     dir=$(pwd)
 fi
 
+# if u is not specified, program wi'll exit.
 if [ -z "$url" ] 
 then
     # Empty url, exit
-    echo "u flag is not defined"
+    echo "INPUT ERROR: u flag is not defined."
     exit 1
 fi
 
@@ -42,15 +47,17 @@ cd $dir
 git init 1>/dev/null 2>/dev/null
 echo "Initialized git repo at: $dir"
 
-# Create a text file and make the first commit
-echo "{\"date\": \"$(date +%d-%m-%y_%H:%M:%S)\", \"count\": \"0\"}" 1> commits.json
+# Create a json file and make the first commit
+echo "{\"date\": \"$(date +%d-%m-%y_%H:%M:%S)\", \"count\": \"0\"}" 1> commits.json 2>/dev/null
 cp $wd/gac.sh $wd/update.sh $dir/
 git add . 1>/dev/null 2>/dev/null
 git commit -m "first commit." 1>/dev/null 2>/dev/null
 
+# Connect to GitHub
 git remote add origin $url 1>/dev/null 2>/dev/null
 git push -u origin master 1>/dev/null 2>/dev/null
 
+# Add the crontab scheduler
 crontab -l > cron
 echo "0 22 * * * cd $dir; $dir/update.sh" >> cron
 crontab cron
--- update.sh
+++ update.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
+# Author: Mohammad Ghahremani(libnist)
+# Creation date: 07/04/2023
+# Last modifier: 07/04/2023
 
+# Description:
+# Updates a json file and commits it to github.
+
+# Usage:
+# just run the script.
+
+# Reading last_date and count
 last_date=$(grep -o '"date": "[^"]*' commits.json | grep -o '[^"]*$')
 count=$(grep -o '"count": "[^"]*' commits.json | grep -o '[^"]*$')
 
+# Updating the file
 count=$(expr $count + "1")
 current_date=\"$(date +%d-%m-%y_%H:%M:%S)\"
 output="{\"date\": $current_date, \"count\": \"$count\"}"
 
 echo $output 1> commits.json
 
-
+# Commiting and pushing it into the remote repo.
 git add .
 git commit -m "Commited for $count times. last commit was: $last_date"
 git push -u origin master
